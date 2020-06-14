5b5c6ad4254e835ccd30a719a280326378ad0448
Dylan Maccora <maccora17@gmail.com>
2020-06-14 11:31:49 -0700

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 extern crate mover;
 extern crate serde_json;
+extern crate structopt;
 
 use std::fs::File;
 use std::io::Read;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "mover")]
+struct Opt {
+    #[structopt(short = "p", long = "port", default_value="5212")]
+    port: usize,
+
+    #[structopt(short = "d", long = "database", default_value = "db.json")]
+    database_file: String
+}
 
 fn main() {
+    let opt = Opt::from_args();
+
     let mut content = String::new();
-    let mut file = File::open("db.json").expect("Unable to open db.json");
-    file.read_to_string(&mut content)
-        .expect("Unable to read db.json");
+    File::open(&opt.database_file).and_then(|mut f| f.read_to_string(&mut content))
+        .expect(&format!("Unable to read {}", &opt.database_file));
+
+    let url = &format!("127.0.0.1:{}", opt.port).parse().unwrap();
+
     mover::start(
         serde_json::from_str(&content).expect("Failed to parse database JSON"),
-        &"127.0.0.1:5212".parse().unwrap(),
+        url,
     );
 }
